34bdf8af990fdc4c510e28019b9392dc057010e1
Renato <rverissimo.dev@gmail.com>
2025-01-23 12:17:42 -0300

    Preparando para testes

--- src/test/java/br/com/serasa/scoreapp/security/controllers/AuthControllerTest.java
+++ src/test/java/br/com/serasa/scoreapp/security/controllers/AuthControllerTest.java
@@ -1,9 +1,7 @@
 package br.com.serasa.scoreapp.security.controllers;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuthControllerTest {
 
 
